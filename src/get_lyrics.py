43fdd03e268d88f3db7b1326bf0c96914641602c
Davide Lo Re <boyska@gmail.com>
2010-09-19 23:48:50 +0200

    Implement a working, plugin-based get_lyrics
    It is rather basic and inefficient, still.

--- src/get_lyrics.py
+++ src/get_lyrics.py
@@ -1,17 +1,55 @@
 '''
-This module is temporary.
-It will be replaced with one that can handle plugins.
-See :doc:`plans` for more details
+Main module: provide simple, ready-to-use functions
+to get lyrics
 '''
-def get_lyrics(artist=None, album=None, title=None, otherinfo=None,\
-        timeout=-1, filename=None):
-    '''
-    It should get lyrics. But it doesn't, LOL
 
-    .. todo :: write a working get_lyrics
-    :param artist: It specifies the artist
-    :type artist: a string
-    :returns: None
-    :rtype: still to be decided
+import pluginsystem
+
+
+def get_lyrics(artist=None, album=None, title=None, otherinfo=None, \
+        request=(), timeout=-1, filename=None):
     '''
-    return None
+    Simply get_lyrics
+
+    .. todo :: use multiprocessing
+    :param otherinfo: Other metadata, not worthing a function parameter
+    :type otherinfo: dict
+    :param request: all needed metadata. If empty, all will be searched
+    :type request: tuple
+    :param timeout: currently not supported
+    :returns: all the metadata found, as a type:value dict
+    :rtype: dict
+    '''
+    song_metadata = otherinfo if otherinfo else {}
+    if artist:
+        song_metadata['artist'] = artist
+    if album:
+        song_metadata['album'] = album
+    if title:
+        song_metadata['title'] = title
+
+    options = {}
+    options['searching'] = request or ('lyrics', 'coverart')
+
+    res = {}
+    for name, plugin in pluginsystem.get_plugins().items():
+        try:
+            result = plugin.get_data(song_metadata, options)
+        except:
+            print 'WARNING! Plugin %s raised an exception' % name
+        else:
+            #TODO: add deeper checking
+            if type(result) is dict:
+                res[name] = result
+            else:
+                print 'WARNING! Plugin %s returned inconsistent output (%s)' %\
+                        (name, result)
+                print '\tIt probably is a plugin bug'
+
+    first_res = {}
+    for plugin, plugin_res in res.items():
+        for key, value in plugin_res.items():
+            if key not in first_res:
+                first_res[key] = value
+
+    return first_res
